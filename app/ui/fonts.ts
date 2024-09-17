93845f685aecef3cdb8f7deb0037d2e4c4b3847d
Jamal Haider <jamal.hyderr@gmail.com>
2024-09-17 20:22:19 +0500

    dashboard base layout

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+
+import { Lusitana } from 'next/font/google';
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
+
+import { Nunito } from 'next/font/google';
+export const nunito = Nunito({ subsets: ['latin'], weight: ['400', '700'] });
